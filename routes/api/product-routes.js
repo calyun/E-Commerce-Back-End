08f95d833f8e620a60cf85cf02fca974aca3d22b
calyun <cmyounk@gmail.com>
2021-11-05 00:06:50 -0400

    Finalizing All Routes
    Everybody Work

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -31,21 +31,34 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findByPk(req.params.id, {
-    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-    // include: [
-    //   {
-    //     model: Category,
-    //     include: ['category_name']
-    //   },
-    //   {
-    //     model: Tag,
-    //     include: ['tag_name']
-    //   }
-    // ]
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
   })
-  .then(productData => res.json(productData));
-});
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({message: 'No product found with this id'});
+        return;
+      }
+      res.json(productData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
 
 // create new product
 router.post('/', (req, res) => {
